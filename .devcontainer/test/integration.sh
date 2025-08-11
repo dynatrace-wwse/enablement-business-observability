6ebff752c564da41c75f4447efc1f99d68ea2836
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2025-08-11 12:43:58 +0100

    Comparing to Framework V.0.1 -> Codespaces-synchronizer

--- .devcontainer/test/integration.sh
+++ .devcontainer/test/integration.sh
@@ -7,10 +7,12 @@ source $REPO_PATH/.devcontainer/test/test_functions.sh
 
 printInfoSection "Running integration Tests for the Enablement Framework"
 
-assertRunningPod dynatrace operator
+assertRunningPod astroshop adservice
 
-assertRunningPod dynatrace activegate
+assertRunningPod astroshop cartservice
 
-assertRunningPod dynatrace oneagent
+assertRunningPod astroshop checkoutservice
+
+assertRunningPod cert-manager cainjector
 
 assertRunningApp 30100
